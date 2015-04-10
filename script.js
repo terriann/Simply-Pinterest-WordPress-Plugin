6d5a6c1e6dfca3432454ff9b803bbfc1e3f81ce6
Terri Ann <terri.ann.s@gmail.com>
2015-04-10 12:54:11 -0400

    Update script to loop more efficients resolves #5

--- script.js
+++ script.js
@@ -1,73 +1,83 @@
 jQuery(document).ready(function() {
 
-    jQuery('.bpp_post_wrapper img').each(function(){
+    var version = '0.1.5';
 
-        // Respect the no-pin
-        // Exclude small images
-        if( jQuery(this).attr('nopin') === 'nopin'
-         || jQuery(this).get(0).clientWidth < 200) {
-            return;
-        }
+    // Loop through each post
+    jQuery('.bpp_post_wrapper').each(function(){
 
-        // Grab the post wrapper
-        // @todo    ********************************
-        // Could probably re-organize to address each post individually then loop through each image
-        var bpp_post_wrapper = jQuery(this).closest('div.bpp_post_wrapper');
+        var $bpp_post = jQuery(this);
 
-        // Create the pinterest URI for this image
-        var bpp_href  = '//www.pinterest.com/pin/create/button/';
-            bpp_href += '?url=' + encodeURI(jQuery(bpp_post_wrapper).data('bpp-pinlink'));
-            bpp_href += '&media=' + encodeURI(jQuery(this).attr('src'));
-            bpp_href += '&description=' + encodeURI(jQuery(this).attr('alt'));
+        // get configured setting
+        var bpp_color = $bpp_post.data('bpp-color'),
+            bpp_size = $bpp_post.data('bpp-size'),
+            bpp_lang = $bpp_post.data('bpp-lang'),
+            bpp_count = $bpp_post.data('bpp-count'),
+            bpp_pinhover = $bpp_post.data('bpp-pinhover'),
+            bpp_pincorner = $bpp_post.data('bpp-pincorner'),
+            bpp_pinlink = $bpp_post.data('bpp-pinlink');
 
-        var bpp_button_wrap = jQuery('<span>')
-                              .addClass('bpp_button_wrapper');
+        // Loop through each image in a post
+        $bpp_post.find('img').each(function(){
 
-        // get color setting
-        var bpp_color = jQuery(bpp_post_wrapper).data('bpp-color');
-        var bpp_size = jQuery(bpp_post_wrapper).data('bpp-size');
-        var bpp_lang = jQuery(bpp_post_wrapper).data('bpp-lang');
-        var bpp_count = jQuery(bpp_post_wrapper).data('bpp-count');
+            // Respect the no-pin
+            // Exclude small images
+            if( jQuery(this).attr('nopin') === 'nopin'
+             || jQuery(this).get(0).clientWidth < 200) {
+                return;
+            }
 
-        // Do not change these to data() cause then the number doesn't load
-        var bpp_anchor = jQuery('<a>')
-                         .attr('href', bpp_href)
-                         .attr('data-pin-do', 'buttonPin')
-                         .attr('data-pin-config', bpp_count)
-                         .attr('data-pin-height', bpp_size)
-                         .attr('data-pin-lang', bpp_lang)
-                         .attr('data-pin-color', bpp_color);
+            // Create the pinterest URI for this image
+            var bpp_href  = '//www.pinterest.com/pin/create/button/';
+                bpp_href += '?url=' + encodeURI(bpp_pinlink);
+                bpp_href += '&media=' + encodeURI(jQuery(this).attr('src'));
+                bpp_href += '&description=' + encodeURI(jQuery(this).attr('alt'));
 
-        var bpp_button = jQuery('<img>').attr('src', '//assets.pinterest.com/images/pidgets/pinit_fg_'+bpp_lang+'_rect_'+bpp_color+'_'+bpp_size+'.png');
+            var bpp_button_wrap = jQuery('<span>')
+                                  .addClass('bpp_button_wrapper');
 
-        // Append the button to the anchor inside the button wrapper for display
-        var bpp_display = jQuery(bpp_button_wrap).append(jQuery(bpp_anchor).append(bpp_button));
+            // Do not change these to data() cause then the number doesn't load
+            var bpp_anchor = jQuery('<a>')
+                             .attr('href', bpp_href)
+                             .attr('data-pin-do', 'buttonPin')
+                             .attr('data-pin-config', bpp_count)
+                             .attr('data-pin-height', bpp_size)
+                             .attr('data-pin-lang', bpp_lang)
+                             .attr('data-pin-color', bpp_color);
 
-        // Add miscelaneous classes to image wrapper
-        var bpp_img_wrap_class = '';
-        // If onhover setting is on set class
-        if(jQuery(bpp_post_wrapper).data('bpp-pinhover') == true) {
-            bpp_img_wrap_class += " onhover";
-        }
-        // set the corner the pin belongs
-        if(jQuery(bpp_post_wrapper).data('bpp-pincorner')) {
-            bpp_img_wrap_class += " " + jQuery(bpp_post_wrapper).data('bpp-pincorner');
-        }
-        // If it's the large button
-        if(bpp_size == 28) {
-            bpp_img_wrap_class += " large";
-        }
-        // Add class for count_above, count_none and count_beside
-        bpp_img_wrap_class += " count_"+bpp_count;
+            var bpp_button = jQuery('<img>').attr('src', '//assets.pinterest.com/images/pidgets/pinit_fg_'+bpp_lang+'_rect_'+bpp_color+'_'+bpp_size+'.png');
 
-        // Add wrapper and drop button after image
-        if(jQuery(this).parent('a').length > 0) {
-            jQuery(this).parent('a').wrap('<div class="bpp_img_wrapper' + bpp_img_wrap_class + '"></div>');
-            jQuery(this).parent('a').after(bpp_display);
-        } else {
-            jQuery(this).wrap('<div class="bpp_img_wrapper' + bpp_img_wrap_class + '"></div>');
-            jQuery(this).after(bpp_display);
-        }
+            // Append the button to the anchor inside the button wrapper for display
+            var bpp_display = jQuery(bpp_button_wrap).append(jQuery(bpp_anchor).append(bpp_button));
+
+            // Add miscelaneous classes to image wrapper
+            var bpp_img_wrap_class = '';
+            // If onhover setting is on set class
+            if(bpp_pinhover == true) {
+                bpp_img_wrap_class += " onhover";
+            }
+            // set the corner the pin belongs
+            if(bpp_pincorner) {
+                bpp_img_wrap_class += " " + bpp_pincorner;
+            }
+            // If it's the large button
+            if(bpp_size == 28) {
+                bpp_img_wrap_class += " large";
+            }
+            // Add class for count_above, count_none and count_beside
+            bpp_img_wrap_class += " count_"+bpp_count;
+
+            // Add wrapper and drop button after image
+            if(jQuery(this).parent('a').length > 0) {
+                jQuery(this).parent('a').wrap('<div class="bpp_img_wrapper' + bpp_img_wrap_class + '"></div>');
+                jQuery(this).parent('a').after(bpp_display);
+            } else {
+                jQuery(this).wrap('<div class="bpp_img_wrapper' + bpp_img_wrap_class + '"></div>');
+                jQuery(this).after(bpp_display);
+            }
+
+        // End looping through each image in a post
+        });
+    // End looping through each post
     });
-
+// End on ready
 });
