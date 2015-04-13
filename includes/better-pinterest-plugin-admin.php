bf9fc66b9c3f08cfa0220eaf3591eaffefb6db09
Terri Swallow <terri.ann.s@gmail.com>
2015-04-13 13:17:11 -0700

    Missing include files?

---
+++ includes/better-pinterest-plugin-admin.php
@@ -0,0 +1,118 @@
+<?php
+
+    /**
+     * Class that manages manipulation on the admin in wordpress
+     */
+
+    class Better_Pinterest_Plugin_Admin {
+
+        const VERSION = '0.1';
+
+        public static function admin_init()
+        {
+            // Enqueue editor things - used in post.php and pages.php
+            add_action( 'wp_enqueue_editor', array( __CLASS__, 'enqueue' ), 10, 1 );
+            add_action( 'print_media_templates', array( __CLASS__, 'template' ) );
+
+            // Add settings link under plugin actions on plugins page
+            add_filter( 'plugin_action_links_' . plugin_basename(BPP_PLUGIN_FILE), array( __CLASS__, 'plugin_action_links') );
+        }
+
+        public static function init()
+        {
+            // create custom plugin settings menu
+            add_action('admin_menu', array(  __CLASS__, 'create_settings_menu' ) );
+            // registeres settings so POST knows to look for them
+            add_action('admin_init', array(  __CLASS__, 'register_settings' ) );
+        }
+
+
+        public static function enqueue( $options )
+        {
+
+            if ( $options['tinymce'] ) {
+                // Note: An additional dependency "media-views" is not listed below
+                // because in some cases such as /wp-admin/press-this.php the media
+                // library isn't enqueued and shouldn't be. The script includes
+                // safeguards to avoid errors in this situation
+                wp_enqueue_script( 'advanced-pinterest-settings', plugins_url( 'scripts/advanced-pinterest-settings.js', BPP_PLUGIN_FILE), array( 'jquery' ), self::VERSION, true );
+            }
+        }
+
+        /**
+         * Backbone template for advanced settings through editing an image in the visual editor for posts/pages
+         * @return null
+         */
+        public static function template()
+        {
+            include( plugin_dir_path(BPP_PLUGIN_FILE) . 'templates/advanced-pinterest-settings-tmpl.php');
+        }
+
+        /**
+         * Register settings so wordpress knows to update them when editing plugin settings
+         * @return null
+         */
+        public static function register_settings()
+        {
+            //register our settings
+            register_setting( 'bpp-settings-group', 'bpp_color' );
+            register_setting( 'bpp-settings-group', 'bpp_onhover' );
+            register_setting( 'bpp-settings-group', 'bpp_corner' );
+            register_setting( 'bpp-settings-group', 'bpp_size', 'intval' );
+            register_setting( 'bpp-settings-group', 'bpp_lang' );
+            register_setting( 'bpp-settings-group', 'bpp_count' );
+            register_setting( 'bpp-settings-group', 'bpp_loadasync' );
+        }
+
+
+        public static function settings_default()
+        {
+            // Set default values
+            self::update_option('bpp_color', 'red');
+            self::update_option('bpp_onhover', 'false');
+            self::update_option('bpp_corner', 'northeast');
+            self::update_option('bpp_size', 20);
+            self::update_option('bpp_lang', 'en');
+            self::update_option('bpp_count', 'above');
+            self::update_option('bpp_loadasync', 'false');
+        }
+
+        public static function update_option($name, $value)
+        {
+            if (get_option($name) !== false) {
+                update_option( $name, $new_value );
+            } else {
+                add_option( $name, $value);
+            }
+        }
+
+        public static function settings_remove()
+        {
+            delete_option('bpp_color');
+            delete_option('bpp_onhover');
+            delete_option('bpp_corner');
+            delete_option('bpp_size');
+            delete_option('bpp_lang');
+            delete_option('bpp_count');
+            delete_option('bpp_loadasync');
+        }
+
+        /**
+         * Add submenu under settings
+         * @return null
+         */
+        public static  function create_settings_menu()
+        {
+            add_submenu_page( 'options-general.php', 'Better Pinterest Plugin Settings', 'Pinterest Settings', 'update_plugins', 'settings_bpp', array( __CLASS__, 'settings_page' ) );
+        }
+
+        public static function settings_page() {
+            include('settings-page.php');
+        }
+
+        public static function plugin_action_links( $links ) {
+           $links[] = '<a href="'. get_admin_url(null, 'options-general.php?page=settings_bpp') .'">Settings</a>';
+           $links[] = '<a href="https://github.com/terriann/betterpinterestplugin/wiki" target="_blank">Wiki</a>';
+           return $links;
+        }
+    }
---
+++ includes/better-pinterest-plugin.php
@@ -0,0 +1,91 @@
+<?php
+
+    /**
+     * Class that manages manipulation for the client facing side of Wordpress
+     */
+
+    class Better_Pinterest_Plugin {
+
+        const VERSION = '0.1';
+
+        public static function init()
+        {
+            // Enqueue frontend things
+            add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue' ), 10, 1 );
+            add_action( 'wp_head', array( __CLASS__, 'meta_nohover' ) );
+            // Wrap the posts with the postwrapper class - JS is going to use a bunch of these data attributes
+            add_filter( 'the_content', array( __CLASS__, 'wrap_post_content' ) );
+        }
+
+        public static function enqueue()
+        {
+            wp_enqueue_style( 'bpp_css', plugins_url( '/styles/style.css', BPP_PLUGIN_FILE ), false, self::VERSION );
+            wp_enqueue_script( 'bpp_js', plugins_url( '/scripts/script.js', BPP_PLUGIN_FILE ), array( 'jquery' ), self::VERSION );
+
+            if(!self::loadAsync()){
+                wp_enqueue_script( 'bpp_pinit', '//assets.pinterest.com/js/pinit.js', false, self::VERSION );
+            } else {
+                add_action( 'wp_footer', array(__CLASS__, 'async_script'));
+            }
+        }
+
+        /**
+         * This appears to be the right way to prevent the pinterest plugin button from hovering as well
+         * https://developers.pinterest.com/extension_faq/
+         * @return [type] [description]
+         */
+        public static function meta_nohover()
+        {
+            echo '<meta name="pinterest" content="nohover" />';
+        }
+
+        public static function async_script()
+        {
+            echo '<script type="text/javascript">';
+            echo "(function(d){
+                var f = d.getElementsByTagName('SCRIPT')[0], p = d.createElement('SCRIPT');
+                p.type = 'text/javascript';
+                p.async = true;
+                p.src = '//assets.pinterest.com/js/pinit.js';
+                f.parentNode.insertBefore(p, f);
+            }(document));
+            </script>";
+        }
+
+        public static function loadAsync()
+        {
+            if(get_option('bpp_loadasync') == 'true') {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static function wrap_post_content( $content )
+        {
+            global $post;
+
+            $attr_set = array(
+                    'class' => 'bpp_post_wrapper',
+                    'data-bpp-pinlink' => esc_attr( get_permalink($post->ID) ),
+                    'data-bpp-pincorner' => esc_attr( get_option('bpp_corner') ),
+                    'data-bpp-pinhover' => esc_attr( get_option('bpp_onhover') ),
+                    'data-bpp-lang' => esc_attr( get_option('bpp_lang') ),
+                    'data-bpp-count' => esc_attr( get_option('bpp_count') ),
+                    'data-bpp-size' => esc_attr( get_option('bpp_size') ),
+                    'data-bpp-color' => esc_attr( get_option('bpp_color') )
+                    );
+
+            $attributes = array();
+
+            foreach($attr_set as $attr => $value) {
+                if(!empty($value)) {
+                    $attributes[] = $attr . '="' . $value . '"';
+                }
+            }
+
+            $content = '<div '. implode(' ', $attributes) .'>'.$content.'</div>';
+            return $content;
+        }
+
+    }
