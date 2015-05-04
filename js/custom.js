e9ffbdfd7548991c77656b782b3a4e223eee052a
Terri Ann <terri.ann.s@gmail.com>
2015-05-04 17:23:33 -0400

    Update the github page to be way more awesome, still a work in progress, needs videos and screenshots

---
+++ js/custom.js
@@ -0,0 +1,63 @@
+$( document ).ready(function() {
+
+    $('.next-btn').each(function(){
+        $(this).on('click', function(e){
+            e.preventDefault();
+            var currentpane = $(this).data('currentpane');
+            var togglepane = $(this).data('togglepane');
+
+            $('#'+currentpane).hide();
+            $('#' + togglepane).removeClass('hidden').show();
+
+        });
+    });
+
+
+    $("a[href^='#']").click(function(e) {
+        e.preventDefault();
+        var anchor = $(this).attr('href').substring(1);
+        ga('send', 'event', 'anchor', anchor);
+        $('html, body').animate({
+            scrollTop: $("a[name='"+anchor+"']").offset().top
+        }, 2000);
+    });
+
+
+
+
+
+
+
+    // Event tracking
+
+    // Tracking downloads
+    $('.intro-message a.download').on('click', function() {
+        ga('send', 'event', 'button', 'download', 'header');
+    });
+    $('#page-end a.download').on('click', function() {
+        ga('send', 'event', 'button', 'download', 'footer');
+    });
+    $('#install-guide .ftp-instructions a.download').on('click', function() {
+        ga('send', 'event', 'link', 'download', 'ftp-install');
+    });
+    $('#install-guide .admin-instructions a.download').on('click', function() {
+        ga('send', 'event', 'link', 'download', 'ftp-install');
+    });
+
+    // Tips next btn
+    $('#pinterest-tips button').on('click', function() {
+        ga('send', 'event', 'button', 'pinterest-tips', $(this).data('togglepane'));
+    });
+
+    // Config next button
+    $('#config-options button').on('click', function() {
+        ga('send', 'event', 'button', 'config-options', $(this).data('togglepane'));
+    });
+
+    // external clicks
+    $("a[href^='htt']").on('click', function() {
+        ga('send', 'event', 'external', 'click',  $(this).attr('href'));
+    });
+
+
+});
