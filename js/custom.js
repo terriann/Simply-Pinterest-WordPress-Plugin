ef82f6d44026a765123a7e3cc77507eb420cfc94
Terri Ann <terri.ann.s@gmail.com>
2015-05-05 12:08:42 -0400

    Resolves #18

--- js/custom.js
+++ js/custom.js
@@ -61,3 +61,9 @@ $( document ).ready(function() {
 
 
 });
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+});
