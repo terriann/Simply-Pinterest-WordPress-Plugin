9cb089b40f6134dd6f7f7552245157aa6e8cd952
Terri Ann <terri.ann.s@gmail.com>
2017-12-09 14:02:41 -0500

    Continuing to refacrot and clean up code

--- uninstall.php
+++ uninstall.php
@@ -1,14 +1,23 @@
 <?php
-// If uninstall is not called from WordPress, exit
-if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-    exit();
+/**
+ * Simply Pinterest Uninstall File Doc Comment
+ *
+ * @category  Uninstall
+ * @package   SimplyPinterest
+ * @author    Terri Ann Swallow
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link      http://www.terrianncreative.com/
+ */
+
+// If uninstall is not called from WordPress, exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit();
 }
 
-define('BPP_PLUGIN_FILE', __FILE__);
+define( 'BPP_PLUGIN_FILE', __FILE__ );
 
-require_once( dirname(__FILE__) . '/includes/simple-pinterest-base.php' );
-require_once( dirname(__FILE__) . '/includes/class-simple-pinterest-plugin-admin.php' );
+require_once __DIR__ . '/includes/simple-pinterest-base.php';
+require_once __DIR__ . '/includes/class-simple-pinterest-plugin-admin.php';
 
-// When the plugin is uninstalled, remove options from database
-
+// When the plugin is uninstalled, remove options from database.
 Simple_Pinterest_Plugin_Admin::settings_remove();
